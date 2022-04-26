15d5ecc144bbd4146afcfca99b574ec65c86f914
Abdallah Ismaili <89218851+AbdallahIsmaili@users.noreply.github.com>
2022-04-26 02:23:54 +0100

    update

---
+++ script/main.js
@@ -0,0 +1,10 @@
+let rel_par = document.getElementById('relation');
+let personName = document.getElementById('main-header');
+let searched = document.getElementById('name-input')
+
+let names = [[], [], [], [], [], [], [], [], [], [], []]
+
+function findMyName() {
+    
+}
+
